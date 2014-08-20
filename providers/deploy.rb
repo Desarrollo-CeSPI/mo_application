a652e074df6876edc0058b1f8e452edec15c6034
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-08-20 20:46:44 -0300

    Refactor code

--- providers/deploy.rb
+++ providers/deploy.rb
@@ -1,31 +1,34 @@
 use_inline_resources
 
-action :install do
-  create_user
-  create_directories
-  if new_resource.callbacks[:before_deploy].respond_to?(:call)
-    instance_eval(&new_resource.callbacks[:before_deploy])
-  end
-  deploy
+# Support whyrun
+def whyrun_supported?
+   true
 end
 
-
+# Load current resource set defaults for some unsetted attributes
 def load_current_resource
-  @current_resource = Chef::Resource::CespiApplicationDeploy.new(@new_resource.name)
+  @current_resource ||= Chef::Resource::CespiApplicationDeploy.new(@new_resource.name)
 end
 
-def initialize(new_resource, run_context)
-  new_resource.user new_resource.name unless new_resource.user
-  new_resource.group new_resource.user unless new_resource.group
-  new_resource.home "/home/#{new_resource.user}" unless new_resource.home
-  super(new_resource, run_context)
+# Installs an application
+action :install do
+  converge_by("Install #{ @new_resource }") do
+    install
+  end
 end
 
 
 private
-def create_user
-  new_resource = @new_resource
 
+def install
+  create_user
+  create_directories
+  callback :before_deploy
+  deploy
+end
+
+# Creates group & user
+def create_user
   group new_resource.group
 
   user new_resource.user do
@@ -36,34 +39,57 @@ def create_user
   end
 end
 
+# Creates application required directories to deploy into
+# Applications directory tree will be composed as capistrano or
+# Chef deploy resource expects to be:
+# /app/base_dir/
+#   + shared/
+#   + releases/
+#   + @current -> releases/xxx
+# We must create shared folder and every folder containing files to be 
+# written as templates
 def create_directories
-  new_resource = @new_resource
   shared_directories.
     insert(0,new_resource.path).
     insert(1,"#{new_resource.path}/shared").
     each {|dir| create_directory dir }
 end
 
+# Returns every shared directory
+# This directories will be:
+#   * Every shared_files dirname
+#   * Every shared_directory
+# All this specified directories are relative to shared folder, so
+# we need to build full_path
 def shared_directories
-  dirs = @new_resource.shared_files.map do |shared_path, _ |
-    ::File.join @new_resource.path,'shared', ::File.dirname(shared_path)
-  end
-  (dirs + @new_resource.shared_dirs.map do |shared_dir, _ |
-    ::File.join @new_resource.path,'shared', shared_dir
-  end).sort.uniq
+  (
+    new_resource.shared_files.map do |shared_path, _ |
+      ::File.join new_resource.path,'shared', ::File.dirname(shared_path)
+    end +
+    new_resource.shared_dirs.map do |shared_dir, _ |
+      ::File.join new_resource.path,'shared', shared_dir
+    end
+  ).sort.uniq
 end
 
+# Helper to create directories with needed permissions
 def create_directory(dir)
-  new_resource = @new_resource
-  directory dir do 
+  directory dir do
     recursive true
     owner new_resource.user
     group new_resource.group
   end
 end
 
-def deploy(s_action = :deploy)
-  new_resource = @new_resource
+# If there is a callback setted for name, then call it
+def callback(name)
+  if new_resource.callbacks[name].respond_to?(:call)
+    instance_eval(&new_resource.callbacks[name])
+  end
+end
+
+# Chef deploy resource wrapper
+def deploy
   deploy_revision new_resource.name do
     deploy_to new_resource.path
     repo new_resource.repo
@@ -76,6 +102,6 @@ def deploy(s_action = :deploy)
     group new_resource.group
     migrate new_resource.migrate
     migration_command new_resource.migration_command
-    action s_action
+    action (new_resource.force_deploy ? :force_deploy : :deploy)
   end
 end
--- resources/deploy.rb
+++ resources/deploy.rb
@@ -2,9 +2,9 @@ actions :install, :remove
 default_action :install
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :user, :kind_of => [String, NilClass]
-attribute :group, :kind_of => [String, NilClass]
-attribute :home, :kind_of => [String, NilClass]
+attribute :user, :kind_of => [String, NilClass], :default => (lazy {|r|r.name})
+attribute :group, :kind_of => [String, NilClass], :default => (lazy {|r|r.user})
+attribute :home, :kind_of => [String, NilClass], :default => (lazy {|r| "/home/#{r.user}"})
 attribute :shell, :kind_of => String, :default => "/bin/bash"
 attribute :path, :kind_of => String, :required => true
 attribute :repo, :kind_of => String, :required => true
@@ -13,10 +13,17 @@ attribute :migrate, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :migration_command, :kind_of => [String, NilClass]
 attribute :shared_dirs, :kind_of => [Hash], :default => Hash.new
 attribute :shared_files, :kind_of => [Hash], :default => Hash.new
+attribute :force_deploy, :kind_of => [TrueClass,FalseClass], :default => false
 
 attr_accessor :callbacks
 
+
+def initialize(name, run_context=nil)
+  super
+  @callbacks = {}
+end
+
+
 def before_deploy(arg=nil, &block)
-  @callbacks ||={}
-  @callbacks[:before_deploy] = block
+  callbacks[:before_deploy] = block
 end
