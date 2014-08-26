5c1c8430203485708c68adae2938d54da50bd0db
Leandro Di Tommaso <leandro@scarlett.local>
2014-08-26 15:39:15 -0300

    Fixed permissions fot /tmp.
    This fix adds 777 to /tmp.

--- providers/chroot.rb
+++ providers/chroot.rb
@@ -56,6 +56,9 @@ def create
     each do |dir|
       directory ::File.join(new_resource.path, dir) do
         recursive true
+        if dir == "/tmp"
+          mode "1777"
+        end
       end
   end
   copy
