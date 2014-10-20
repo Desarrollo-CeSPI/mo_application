cb57b131f3d1c9c08ab50667c1c5ec58c1ffe514
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-10-20 17:20:21 -0300

    Add nscd to chroot LWRP

--- providers/chroot.rb
+++ providers/chroot.rb
@@ -46,12 +46,18 @@ def copy_dirs
   copy_files.map{|x| ::File.dirname x}.sort.uniq
 end
 
+def nscd_dir
+  '/var/run/nscd'
+end
+
 def chroot_dirs
-  (copy_dirs + %w(/dev /log /run /tmp /var)).sort.uniq
+  (copy_dirs + %w(/dev /log /run /tmp /var) + [nscd_dir]).sort.uniq
 end
 
 
 def create
+  package 'nscd'
+
   chroot_dirs.
     each do |dir|
       directory ::File.join(new_resource.path, dir) do
@@ -61,10 +67,27 @@ def create
         end
       end
   end
+
+  nscd_directory = nscd_dir
+
+  mount ::File.join(new_resource.path, nscd_dir) do
+    device nscd_directory
+    fstype  "none"
+    options "bind"
+    action  [:mount, :enable]
+  end
+
   copy
 end
 
 def remove
+  nscd_directory = nscd_dir
+  mount ::File.join(new_resource.path, nscd_dir) do
+    device nscd_directory
+    fstype  "none"
+    options "bind"
+    action  [:umount, :disable]
+  end
   directory new_resource.path do
     recursive true
     action :delete
