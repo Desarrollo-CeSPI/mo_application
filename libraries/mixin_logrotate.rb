c6de3f61f7fc77a8e125f834f64855d55b83e78d
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-03 14:25:12 -0300

    Add logrotate dependency

--- libraries/mixin_logrotate.rb
+++ libraries/mixin_logrotate.rb
@@ -1,20 +1,49 @@
 class CespiApplication
   module Logrotate
 
-    def application_logs
+    def logrotate_service_logs
+      []
+    end
+
+    def logrotate_application_logs
       ::File.join(new_resource.path, 'shared', new_resource.log_dir, '*.log')
     end
 
+    def logrotate_permissions
+      %W(644 #{new_resource.user} #{new_resource.group}).join ' '
+    end
+
+    def logrotate_options
+      %w(missingok delaycompress notifempty compress sharedscripts)
+    end
+
+    def logrotate_postrotate
+      nil
+    end
+
     def logrotate(enable_flag = true)
-      include_recipe "logrotate"
-      logrotate_app new_resource.name do
-        path application_logs
-        options ['missingok', 'delaycompress', 'notifempty']
-        frequency 'weekly'
-        maxsize   '1M'
-        rotate    10
-        create    %W(644 #{new_resource.user} #{new_resource.group}).join
-        enable enable_flag
+      run_context.include_recipe "logrotate"
+      self.tap do |me|
+        logrotate_app "cespi-application-services-#{new_resource.name}" do
+          path me.logrotate_service_logs
+          options me.logrotate_options
+          frequency 'weekly'
+          maxsize   '1M'
+          rotate    10
+          create me.logrotate_permissions
+          postrotate me.logrotate_postrotate if me.logrotate_postrotate
+          enable enable_flag
+        end unless logrotate_service_logs.empty?
+
+        logrotate_app "cespi-application-#{new_resource.name}" do
+          path me.logrotate_application_logs
+          options me.logrotate_options
+          frequency 'weekly'
+          maxsize   '1M'
+          rotate    10
+          create me.logrotate_permissions
+          enable enable_flag
+        end
       end
     end
 
