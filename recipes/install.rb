9c1acd9099fbc5dc972b91646fbdf19bbeb7bb93
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-15 18:31:29 -0300

    Fix install recipe

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,4 +2,4 @@ include_recipe 'mo_application::_ssh_wrapper'
 include_recipe 'nginx::default'
 include_recipe 'chef-msttcorefonts::default'
 
-node[:mo_application][:packages].each { |p| package }
+node[:mo_application][:packages].each { |p| package  p }
