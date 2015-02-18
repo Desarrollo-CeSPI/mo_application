5309888515f278ff0c45eb2bc077d8ab6c267c47
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-02-18 11:24:22 -0300

    Updated chef-sugar

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chrodriguez@gmail.com leandro.ditommaso@mikroways.net'
 license          'MIT'
 description      'Provides resources for almost any application'
 long_description 'Installs/Configures mo_application'
-version          '0.1.37'
+version          '0.1.38'
 
 depends         'mysql',      '~> 5.6.0'
 depends         'database',   '~> 2.3.1'
@@ -14,5 +14,5 @@ depends         'logrotate',  '~> 1.7.0'
 depends         'hostsfile',  '~> 2.4.5'
 depends         'nginx',      "~> 2.7.4"
 depends         'nginx_conf', "~> 0.2.4"
-depends         'chef-sugar', "~> 2.4.1"
+depends         'chef-sugar', "~> 2.5.0"
 depends         'chef-msttcorefonts', "~> 0.9.0"
