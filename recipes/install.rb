c86023c35681eb4259620ea9961601f35f219d62
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-04-01 12:46:35 -0300

    Add mysql dependency

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,8 +1,11 @@
 include_recipe 'mo_application::ssh_wrapper'
 include_recipe 'nginx::default'
 include_recipe 'chef-msttcorefonts::default'
-include_recipe 'mysql::client'
 include_recipe 'mo_application::backup'
+
+mysql_client 'default' do
+    action :create
+end
 
 node[:mo_application][:packages].each { |p| package  p }
 
