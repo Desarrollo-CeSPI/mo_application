b81a3f6f7954f11cfcb78d2ca70861b9fc2d0e93
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-04-06 01:42:20 -0300

    undo last changes

--- libraries/helper_db.rb
+++ libraries/helper_db.rb
@@ -7,16 +7,13 @@ def load_mysql_gem
     end
 end
 
-def mo_mysql_data_for_cluster(cluster_name)
-  master = search('node',"chef_environment:#{node.chef_environment} AND tags:mysql_master AND cluster_name:#{cluster_name}")
-  host = master.first
-  fail "Did not find a MySQL cluster named #{cluster_name} tagged as mysql_master for environment #{node.chef_environment}"  if host.nil?
-  { 'host' => host.fqdn, 'type' => 'mysql' }
-end
-
 def mo_database_set_superuser_info(database_data)
   if database_data['cluster']
     database_data.merge! mo_mysql_data_for_cluster(database_data['cluster'])
+    master = search('node',"chef_environment:#{node.chef_environment} AND tags:mysql_master AND cluster_name:#{database_data['cluster']}")
+    host = master.first
+    fail "Did not find a MySQL cluster named #{database_data['cluster']} tagged as mysql_master for environment #{node.chef_environment}"  if host.nil?
+    database_data['host'] ||= host.fqdn
   end
   database_data['application_servers'] = (Array(database_data['application_servers']) << node.ipaddress << "127.0.0.1").uniq
 end
@@ -24,7 +21,7 @@ end
 def mo_database(data)
   load_mysql_gem
   (data['databases'] || Hash.new).each do | name, database |
-    mo_database_set_superuser_info(database.merge!('application_servers' => data['application_servers']))
+    mo_database_set_superuser_info(database.merge('application_servers' => data['application_servers']))
     mo_application_database database['name'] do
       username database['username']
       password database['password']
