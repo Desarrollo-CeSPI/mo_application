cccc001437538aca78096c754ecf53b5ea2f46f5
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-04-10 16:39:40 -0300

    change the way application_url is calculated

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,4 +1,11 @@
+# Return default_url if present or first url behind reverse_proxy or http://default_server_name
 def application_url(hash)
-  "#{hash['proxy_ssl'] && hash['proxy_ssl']['enabled'] ? 'https':'http'}://#{Array(hash['server_name']).first}"
+  return hash['default_url'] if hash['default_url']
+  default_server_name = Array(hash['server_name']).first
+  server_names = hash['reverse_proxy'] && hash['reverse_proxy'].map do |name, options| 
+    "#{options && options['ssl'] ? 'https' : 'http'}://#{options && options['server_name'] || default_server_name}"
+  end || []
+  return server_names.first unless server_names.empty?
+  "http://#{default_server_name}"
 end
 
